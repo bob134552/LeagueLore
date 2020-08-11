2ee1bbb439c39da8f1d3d8a2090c87965d865218
bob134552 <bob134552@hotmail.com>
2020-08-11 19:29:02 +0000

    Working on grid.js file
    - testing different javascript in grid.js

--- assets/scripts/grid.js
+++ assets/scripts/grid.js
@@ -1,20 +1,26 @@
-let xhr = new XMLHttpRequest();
-let champData;
-let champNames;
+function getData(cb) {
+  let xhr = new XMLHttpRequest();
 
-xhr.open("GET", "http://ddragon.leagueoflegends.com/cdn/10.16.1/data/en_GB/champion.json");
-xhr.send();
+  xhr.open(
+    "GET",
+    "http://ddragon.leagueoflegends.com/cdn/10.16.1/data/en_GB/champion.json"
+  );
+  xhr.send();
 
-xhr.onreadystatechange = function() {
+  xhr.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-        champData = JSON.parse(this.responseText);
-    };
+      cb(JSON.parse(this.responseText));
+    }
+  };
 }
 
-setTimeout(function() {
-    champNames = champData.data;  
-    var el = document.getElementById("champ-grid");
-    Object.keys(champNames).forEach(key => {
-    el.innerHTML += `<img src="http://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/${champNames[key].id}.png" alt="${champNames[key].id}">`;
-    });
-}, 500);
+function writeToDocument(champData) {
+  let myArray = Array.from(Object.values(champData.data));
+  let el = " ";
+  for (let i = 0; i < Math.min(myArray.length, 20); i++) {
+    el += `<img id="${myArray[i].name}" src="http://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/${myArray[i].id}.png" alt="${myArray[i].id}">`;
+    document.getElementById("champ-grid").innerHTML = el;
+  }
+}
+
+getData(writeToDocument);
