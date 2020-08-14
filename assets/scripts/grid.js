0f233cf0c0668a1c8e7d4d79bd95c6a25110775e
bob134552 <bob134552@hotmail.com>
2020-08-14 11:40:17 +0000

    Pagination
    - working on pagination, back button works.
    - next button doesn't stop.

--- assets/scripts/grid.js
+++ assets/scripts/grid.js
@@ -17,38 +17,37 @@ function getData(cb) {
 function writeToDocument(champData) {
   let myArray = Array.from(Object.values(champData.data));
   let el = " ";
-  for (let i = 0; i < Math.min(myArray.length); i++) {
+  for (let i = 0; i < myArray.length; i++) {
     el += `<div class="item"><img id="${myArray[i].name}" src="http://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/${myArray[i].id}.png" alt="${myArray[i].id}"></div>`;
     document.getElementById("champ-grid").innerHTML = el;
   }
+  
 }
 
-getData(writeToDocument);
+const galleryItems = document.querySelector(".champ-grid").children;
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const page = document.querySelector(".page-num");
+const maxItem = 20;
+let index = 1;
 
+console.log(galleryItems.length);
+let pagination = Math.ceil(galleryItems.length / maxItem);
 
- const galleryItems=document.querySelector(".champ-grid").children;
- const prev=document.querySelector(".prev");
- const next=document.querySelector(".next");
- const page=document.querySelector(".page-num");
- const maxItem=20;
- let index=1;
-  
-  const pagination=Math.ceil(galleryItems.length/maxItem);
-  console.log(pagination)
-
-  prev.addEventListener("click",function(){
+prev.addEventListener("click",function() {
     index--;
     check();
     showItems();
-  })
-  next.addEventListener("click",function(){
+})
+
+next.addEventListener("click",function(){
   	index++;
   	check();
     showItems();  
-  })
+})
 
-  function check(){
-  	 if(index==pagination){
+function check(){
+  	 if(index == pagination) {
   	 	next.classList.add("disabled");
   	 }
   	 else{
@@ -61,17 +60,15 @@ getData(writeToDocument);
   	 else{
   	   prev.classList.remove("disabled");	
   	 }
-  }
+}
 
-  function showItems() {
+function showItems() {
   	 for(let i=0;i<galleryItems.length; i++){
   	 	galleryItems[i].classList.remove("show");
   	 	galleryItems[i].classList.add("hide");
 
 
   	    if(i>=(index*maxItem)-maxItem && i<index*maxItem){
-  	 	  // if i greater than and equal to (index*maxItem)-maxItem;
-  		  // means  (1*8)-8=0 if index=2 then (2*8)-8=8
           galleryItems[i].classList.remove("hide");
           galleryItems[i].classList.add("show");
   	    }
@@ -79,9 +76,11 @@ getData(writeToDocument);
   	 }
 
   	 	
-  }
+}
 
-  window.onload=function(){
+window.onload = function(){
   	showItems();
   	check();
-  }
+};
+
+getData(writeToDocument);
